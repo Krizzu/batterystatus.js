526a74a10b9f3359f65072d35ab0a9b1a8b5c0fd
Krizzu <krzborowy@yahoo.com>
2016-10-19 22:03:49 +0100

    Wrapped into function, attached to window object

--- batterystatus.js
+++ batterystatus.js
@@ -63,151 +63,145 @@
 
 	*/
 
-	var bstatus = document.createElement("div");
+	var BatteryStatusInit = function BatteryStatusInit() {
 
-	bstatus.setAttribute('id', 'batteryStatusMain');
-	//main part of the battery
-	bstatus.style['font-size'] = '16px';
-	bstatus.style['font-weight'] = '900';
-	bstatus.style.color = 'white';
-	bstatus.style.width = '100px';
-	bstatus.style.height = '180px';
-	bstatus.style.background = '#7ede6e';
-	bstatus.style.border = "2px solid #394488";
-	bstatus.style['border-radius'] = '6px';
-	bstatus.style.position = 'fixed';
-	bstatus.style.right = "120px";
-	bstatus.style.top = "-200px";
-	bstatus.style['box-shadow'] = "0px 0px 2px 1px #333";
+	  var bstatus = document.createElement("div");
 
-	//animation stuff
-	bstatus.style.WebkitTransition = "top 0.5s";
-	bstatus.style.MozTransition = "top 0.5s";
+	  bstatus.setAttribute('id', 'batteryStatusMain');
+	  //main part of the battery
+	  bstatus.style['font-size'] = '16px';
+	  bstatus.style['font-weight'] = '900';
+	  bstatus.style.color = 'white';
+	  bstatus.style.width = '100px';
+	  bstatus.style.height = '180px';
+	  bstatus.style.background = '#7ede6e';
+	  bstatus.style.border = "2px solid #394488";
+	  bstatus.style['border-radius'] = '6px';
+	  bstatus.style.position = 'fixed';
+	  bstatus.style.right = "120px";
+	  bstatus.style.top = "50px";
+	  bstatus.style['box-shadow'] = "0px 0px 2px 1px #333";
 
-	//top part of battery
-	var bModelTop = document.createElement('span');
-	bModelTop.style.width = '40px';
-	bModelTop.style.height = '20px';
-	bModelTop.style.background = '#3c3c71';
-	bModelTop.style.position = 'absolute';
-	bModelTop.style.left = '30px';
-	bModelTop.style.top = '-20px';
+	  //animation stuff
+	  bstatus.style.WebkitTransition = "top 0.5s";
+	  bstatus.style.MozTransition = "top 0.5s";
 
-	//battery status (level) indicator (that div with green background :> )
-	var bLevelStatus = document.createElement('span');
-	bLevelStatus.style.width = '100px';
-	bLevelStatus.style['max-height'] = '180px';
-	bLevelStatus.style.position = 'absolute';
-	bLevelStatus.style.background = 'white';
+	  //top part of battery
+	  var bModelTop = document.createElement('span');
+	  bModelTop.style.width = '40px';
+	  bModelTop.style.height = '20px';
+	  bModelTop.style.background = '#3c3c71';
+	  bModelTop.style.position = 'absolute';
+	  bModelTop.style.left = '30px';
+	  bModelTop.style.top = '-20px';
 
-	//Charging indicator
-	//As I wanted to keep this one file specify, I had to draw svg myself.
-	//With little help of http://www.janvas.com/ :)
-	var chargingSing = document.createElement('span');
-	chargingSing.className = "chargingState";
-	chargingSing.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" class="" pageAlignment="none" x="0px" y="0px" width="100px" height="180px" viewBox="0 0 100 180" enable-background="new 0 0 100 180" xml:space="preserve"><defs/><g type="LAYER" locked="true"/><g type="LAYER"><path transform="matrix(1 0 0 1 12.220795892169605 9.242618741976997)" width="53.60718870346601" height="72.55455712451862" stroke-miterlimit="3" fill="#444444" opacity="0.35" d="M30.880756625320373,30.880756625320366 L53.60718870346601,0.23106546854942067 L35.77767645548187,72.78562259306804 L0,72.78562259306804 L30.880756625320373,30.880756625320366 Z "/><path transform="matrix(-1 0 0 -1 87.89473684210661 137.25288831835712)" width="53.60718870346601" height="72.55455712451862" stroke-miterlimit="3" fill="#444444" opacity="0.35" d="M30.880756625320373,30.880756625320366 L53.60718870346601,0.23106546854942067 L35.77767645548187,72.78562259306804 L0,72.78562259306804 L30.880756625320373,30.880756625320366 Z "/></g></svg>';
+	  //battery status (level) indicator (that div with green background :> )
+	  var bLevelStatus = document.createElement('span');
+	  bLevelStatus.style.width = '100px';
+	  bLevelStatus.style['max-height'] = '180px';
+	  bLevelStatus.style.position = 'absolute';
+	  bLevelStatus.style.background = 'white';
 
-	//adding battery elements to to main component
-	bstatus.appendChild(bModelTop);
-	bstatus.appendChild(bLevelStatus);
+	  //Charging indicator
+	  //As I wanted to keep this one file specify, I had to draw svg myself.
+	  //With little help of http://www.janvas.com/ :)
+	  var chargingSing = document.createElement('span');
+	  chargingSing.className = "chargingState";
+	  chargingSing.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" class="" pageAlignment="none" x="0px" y="0px" width="100px" height="180px" viewBox="0 0 100 180" enable-background="new 0 0 100 180" xml:space="preserve"><defs/><g type="LAYER" locked="true"/><g type="LAYER"><path transform="matrix(1 0 0 1 12.220795892169605 9.242618741976997)" width="53.60718870346601" height="72.55455712451862" stroke-miterlimit="3" fill="#444444" opacity="0.35" d="M30.880756625320373,30.880756625320366 L53.60718870346601,0.23106546854942067 L35.77767645548187,72.78562259306804 L0,72.78562259306804 L30.880756625320373,30.880756625320366 Z "/><path transform="matrix(-1 0 0 -1 87.89473684210661 137.25288831835712)" width="53.60718870346601" height="72.55455712451862" stroke-miterlimit="3" fill="#444444" opacity="0.35" d="M30.880756625320373,30.880756625320366 L53.60718870346601,0.23106546854942067 L35.77767645548187,72.78562259306804 L0,72.78562259306804 L30.880756625320373,30.880756625320366 Z "/></g></svg>';
 
-	//============ Functions
+	  //adding battery elements to to main component
+	  bstatus.appendChild(bModelTop);
+	  bstatus.appendChild(bLevelStatus);
+
+	  var BatteryStatus;
+
+	  return BatteryStatus = {
+
+	    //hide those functions from user
+	    updateBatteryLevel: function updateBatteryLevel(battery) {
+	      var level = document.getElementById('chargingLevel');
+	      level.innerHTML = Number(battery.level * 100).toFixed(0) + '%';
+	      bLevelStatus.style.height = 100 - battery.level * 100 + "%";
+	    },
+
+	    displayChargingIndicators: function displayChargingIndicators(battery) {
+	      var eleArray = document.getElementsByClassName('chargingState');
+	      var _iteratorNormalCompletion = true;
+	      var _didIteratorError = false;
+	      var _iteratorError = undefined;
+
+	      try {
+	        for (var _iterator = eleArray[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+	          var ele = _step.value;
+
+	          if (battery.charging) ele.style.display = 'block';else ele.style.display = 'none';
+	        }
+	      } catch (err) {
+	        _didIteratorError = true;
+	        _iteratorError = err;
+	      } finally {
+	        try {
+	          if (!_iteratorNormalCompletion && _iterator.return) {
+	            _iterator.return();
+	          }
+	        } finally {
+	          if (_didIteratorError) {
+	            throw _iteratorError;
+	          }
+	        }
+	      }
+	    },
+
+	    init: function init() {
+	      var _this = this;
+
+	      navigator.getBattery().then(function (battery) {
+
+	        // Initial module load
+
+	        //after getting battery status, adjust green background with charge level
 
 
-	function updateBatteryLevel(battery) {
-	  bLevelStatus.style.height = 100 - battery.level * 100 + "%";
-	}
+	        //Battery info: level and show proper text/image if charging
+	        var batteryInfo = "";
+	        var enableActivationByKey = false;
 
-	function displayChargingIndicators(battery) {
-	  var eleArray = document.getElementsByClassName('chargingState');
-	  var _iteratorNormalCompletion = true;
-	  var _didIteratorError = false;
-	  var _iteratorError = undefined;
+	        batteryInfo += '<h3 class="chargingState" style="position: absolute;top:120px;left:10px">Charging</h3>';
+	        batteryInfo += '<h3 id="chargingLevel" style="position: absolute;top:140px;left:30px">' + Number(battery.level * 100).toFixed(0) + '%' + '</h3>';
 
-	  try {
-	    for (var _iterator = eleArray[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-	      var ele = _step.value;
+	        bstatus.appendChild(chargingSing);
+	        bstatus.innerHTML += batteryInfo;
+	        document.getElementsByTagName('body')[0].appendChild(bstatus);
 
-	      if (battery.charging) ele.style.display = 'block';else ele.style.display = 'none';
+	        _this.updateBatteryLevel(battery);
+	        _this.displayChargingIndicators(battery);
+
+	        //events here:
+
+	        //on charger plug/unplug event change
+	        battery.onchargingchange = _this.displayChargingIndicators.bind(null, battery);
+
+	        //on level change
+	        battery.onlevelchange = _this.updateBatteryLevel.bind(null, battery);
+
+	        //When key pressed, show battery
+	        if (enableActivationByKey) {
+	          document.addEventListener('keypress', function (event) {
+	            if (event.code == 'KeyB') {
+	              var mainEle = document.getElementById('batteryStatusMain');
+	              var top = Number(mainEle.style.top.replace("px", ""));
+	              if (top == '-200') top = '50';else top = '-200';
+	              mainEle.style.top = top + 'px';
+	            }
+	          });
+	        }
+	      });
 	    }
-	  } catch (err) {
-	    _didIteratorError = true;
-	    _iteratorError = err;
-	  } finally {
-	    try {
-	      if (!_iteratorNormalCompletion && _iterator.return) {
-	        _iterator.return();
-	      }
-	    } finally {
-	      if (_didIteratorError) {
-	        throw _iteratorError;
-	      }
-	    }
-	  }
-	}
 
-	navigator.getBattery().then(function (battery) {
+	  };
+	};
 
-	  // Initial module load
-
-	  //after getting battery status, adjust green background with charge level
-
-
-	  //Battery info: level and show proper text/image if charging
-	  var batteryInfo = "";
-	  var enableActivationByKey = true;
-
-	  batteryInfo += '<h3 class="chargingState" style="position: absolute;top:120px;left:10px">Charging</h3>';
-	  batteryInfo += '<h3 id="chargingLevel" style="position: absolute;top:140px;left:30px">' + battery.level * 100 + '%' + '</h3>';
-
-	  bstatus.appendChild(chargingSing);
-	  bstatus.innerHTML += batteryInfo;
-	  document.getElementsByTagName('body')[0].appendChild(bstatus);
-
-	  updateBatteryLevel(battery);
-	  displayChargingIndicators(battery);
-
-	  //events here:
-
-	  //on charger plug/unplug event change
-	  battery.onchargingchange = displayChargingIndicators.bind(null, battery);
-
-	  //on level change
-	  battery.onchargingchange = updateBatteryLevel.bind(null, battery);
-
-	  //When key pressed, show battery
-	  if (enableActivationByKey) {
-	    document.addEventListener('keypress', function (event) {
-	      if (event.code == 'KeyB') {
-	        var mainEle = document.getElementById('batteryStatusMain');
-	        var top = Number(mainEle.style.top.replace("px", ""));
-	        if (top == '-200') top = '50';else top = '-200';
-	        mainEle.style.top = top + 'px';
-	      }
-	    });
-	  }
-	});
-
-	/*
-	 //properties
-	 battery.chargingTime - Time left to fully charged
-	battery.dischargingTime - Time left to discharge
-	battery.level - Battery level (0.0 - 1.0)
-	battery.charging - Is battery charging? (true, false)
-	 //events
-	 battery.onchargingchange = function() {
-	  console.log('Charging mode changed');
-	}
-	 battery.onchargingtimechange = function() {
-	  console.log('Charging time changed');
-	}
-	 battery.ondischargingtimechange = function() {
-	  console.log('Discharge time changed');
-	}
-	 battery.onlevelchange = function() {
-	  console.log('Battery level changed');
-	}
-	 */
+	window.BatteryStatus = BatteryStatusInit();
 
 /***/ }
 /******/ ]);
