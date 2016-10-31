2d24334737c82fd60b233cf0ad4a790be87907dd
Krizzu <krzysztoff.borowy@gmail.com>
2016-10-31 11:40:09 +0000

    Heroku update

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname+'/'));
+app.use(express.static(__dirname));
 
 app.set('port', process.env.PORT || 8080);
 
 app.get('/', function(req, res) {
-  res.sendFile('index.html');
+  res.sendFile('/index.html');
 })
 
 app.listen(app.get('port'), function () {
